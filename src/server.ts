2e825f030fe345bb93ad220d839320894e8f31b3
Arkanic <arkanic@users.noreply.github.com>
2025-01-31 21:47:10 +1300

    finally figured out intents

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-console.log("hi");
+import {Client, Events, GatewayIntentBits} from "discord.js";
+
+import token from "../config";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on(Events.MessageCreate, async message => {
+    console.log(message.content);
+});
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
